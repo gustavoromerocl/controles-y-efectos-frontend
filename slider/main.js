e2bae3b299589e5f168ec60f0b3a3aaadf1e91f5
romerodevelopment <64169519+romerodevelopment@users.noreply.github.com>
2021-08-28 09:27:45 -0400

    slider - controles del slider e indicadores

--- slider/main.js
+++ slider/main.js
@@ -3,23 +3,44 @@ class Slider{
     this.slider = document.querySelector(selector);
     this.interval = null;
     this.contador = 0;
-    this.start()
+    this.itemsCount =  this.slider.querySelectorAll(".container > *").length;
+    
+    this.start();
+    this.buildControls();
   }
 
   start(){
     this.interval = window.setInterval(this.move, 5000)
   }
 
+  buildControls(){
+    for(let i= 0; i < this.itemsCount; i++ ){
+      let control = document.createElement("li");
+
+      if(i == 0) control.classList.add("active");
+
+      this.slider.querySelector(".controls ul").appendChild(control);
+    }
+  }
+
   move = () => {
-    let itemsCount =  this.slider.querySelectorAll(".container > *").length;
+    
     this.contador++;
-    if (this.contador > itemsCount - 1) this.contador = 0;
+    if (this.contador > this.itemsCount - 1) this.contador = 0;
     this.moveTo(this.contador)
+  }
+
+  resetIndicator(){
+    this.slider.querySelectorAll(".controls li.active")
+      .forEach(element => { 
+        element.classList.remove("active");
+      });
   }
 
   moveTo(index){
     let left = index * 100;
-
+    this.resetIndicator();
+    this.slider.querySelector(".controls li:nth-child("+(index+1)+")").classList.add("active");
     this.slider.querySelector(".container").style.left = `-${left}%`
   }
 }

df4bf7924db2e604c8b94eea6137978c524a7d61
romerodevelopment <64169519+romerodevelopment@users.noreply.github.com>
2021-08-28 09:51:34 -0400

    slider - mover por controles

--- slider/main.js
+++ slider/main.js
@@ -1,16 +1,49 @@
+class IndexForSiblings{
+  static get(el){
+    let children =  el.parentNode.children;
+
+    for (let i = 0; i < children.length; i++) {
+      let child = children[i];
+      if (child == el) return i;
+    }
+  }
+}
+
 class Slider{
-  constructor(selector){
+  constructor(selector, movimiento=true){
     this.slider = document.querySelector(selector);
     this.interval = null;
     this.contador = 0;
     this.itemsCount =  this.slider.querySelectorAll(".container > *").length;
-    
+    this.movimiento =  movimiento;
+
     this.start();
     this.buildControls();
+    this.bindEvents();
   }
 
   start(){
+    if(!this.movimiento) return;
     this.interval = window.setInterval(this.move, 5000)
+  }
+
+  restart(){
+    if(this.interval) window.clearInterval(this.interval);
+    this.start();
+  }
+
+  bindEvents(){
+    this.slider.querySelectorAll(".controls li")
+      .forEach(element => { 
+        element.addEventListener("click", this.moveByButton);
+      });
+  }
+
+  moveByButton = (ev) => {
+    let index = IndexForSiblings.get(ev.currentTarget);
+    this.contador = index;
+    this.moveTo(index);
+    this.restart();
   }
 
   buildControls(){

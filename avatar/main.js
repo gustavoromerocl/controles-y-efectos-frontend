2837c73b566f9919f482ab3d3d8b34ea948e1dd7
romerodevelopment <64169519+romerodevelopment@users.noreply.github.com>
2021-09-02 08:35:05 -0400

    videos e imagenes - previsualizacion de subida de imagenes

---
+++ avatar/main.js
@@ -0,0 +1,12 @@
+(function(){
+  document.querySelector("#file-uploader")
+    .addEventListener("change", function(ev){
+      let files = ev.target.files;
+      console.log(files)
+      let image = files[0];
+      let imageURL = URL.createObjectURL(image);
+
+      document.querySelector(".profile .img")
+        .style.backgroundImage = `url(${imageURL})`
+    })
+})()

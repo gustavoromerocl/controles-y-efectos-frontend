e8c4101788acdeb5d117428017286296b937da72
romerodevelopment <64169519+romerodevelopment@users.noreply.github.com>
2021-10-03 16:39:48 -0300

    autompletado - datalist para ofrecer opciones

--- autocomplete/main.js
+++ autocomplete/main.js
@@ -1,4 +1,33 @@
-class Autocomplete{}
+class Autocomplete{
+  constructor(input_selector, base_url){
+    this.input = document.querySelector(input_selector)
+    this.url = base_url;
+    this.buildDataList();
+  }
+
+  buildDataList(){
+    this.dataList = document.createElement("datalist");
+    this.dataList.id = "datalist-autocomplete";
+    document.querySelector("body").appendChild(this.dataList)
+    this.input.setAttribute("list", "datalist-autocomplete")
+  }
+
+  build(response){
+    this.dataList.innerHTML = ""
+    response.items.forEach( item => {
+      let optionEl = document.createElement("option");
+      optionEl.value = item.volumeInfo.title;
+      if (item.volumeInfo.title) optionEl.innerHTML = item.volumeInfo.title;
+      
+      this.dataList.appendChild(optionEl)
+    })
+  }
+
+  search(){
+    Search.get(this.url +"harry")
+    .then(results => this.build(results));
+  }
+}
 
 class Search{
   static get(url){
@@ -26,6 +55,6 @@ class Search{
 
 (function(){
   const googleBooksApiUrl = "https://www.googleapis.com/books/v1/volumes?q=";
-  Search.get(googleBooksApiUrl+"harry")
-  .then(results => console.log(results));
+  let autocomplete = new Autocomplete("#searcher", googleBooksApiUrl);
+  autocomplete.search();
 })();

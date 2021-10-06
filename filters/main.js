f70ed2dfe12b4b8573efd8100e2619ed461f0dcb
romerodevelopment <64169519+romerodevelopment@users.noreply.github.com>
2021-10-06 00:56:52 -0300

    animaciones - filtro de elementos animado

---
+++ filters/main.js
@@ -0,0 +1,11 @@
+/**https://www.kunkalabs.com/mixitup/ */
+(function(){
+  mixitup(".container", {
+    "animation": {
+        "duration": 250,
+        "nudge": false,
+        "reverseOut": false,
+        "effects": "rotateX(180deg)"
+    }
+});
+})()

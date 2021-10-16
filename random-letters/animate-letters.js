690439f1ad7e89cd4f40ccf1ab47221753287619
romerodevelopment <64169519+romerodevelopment@users.noreply.github.com>
2021-10-16 13:09:23 -0300

    efectos con texto - efecto de entrada con letras aleatorias

---
+++ random-letters/animate-letters.js
@@ -0,0 +1,40 @@
+const Animator = {
+  animate: function(options){
+
+    options.stagger = options.stagger || 200;
+    options.staggerPerLetter = options.staggerPerLetter || 50;
+
+    let el = document.querySelector(options.selector)
+    let text = el.innerText;
+    el.innerText = "";
+
+    for (const letter_index in text) {
+      let letter = text[letter_index];
+      console.log(letter);
+      let span = document.createElement('span');
+      span.innerText = " ";
+      el.appendChild(span);
+
+      setTimeout(()=>{
+        this.animateLetter({
+          element: span,
+          letter: letter,
+          stagger: options.staggerPerLetter
+        })
+      },options.stagger*letter_index)
+    }
+  },
+  animateLetter: function(options, contador = 0){
+    if(contador > 20) return options.element.innerText = options.letter
+
+    contador++
+
+    setTimeout(()=>{
+      options.element.innerText = this.getRandomChar();
+      this.animateLetter(options, contador);
+    },options.stagger)
+  },
+  getRandomChar: function(){
+    return Math.random().toString(36).substr(2,1);
+  }
+}
